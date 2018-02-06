b358a52595382480cfc2b01463de853901f48c6b
Alex Taylor <2247116t@student.gla.ac.uk>
2018-02-06 20:00:19 +0000

    chap 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from rango.models import Category
+from rango.models import page
+
 
 
 
 admin.site.register(Category)
+admin.site.register(page)
 
 # Register your models here.
